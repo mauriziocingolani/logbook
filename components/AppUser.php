6be1917256184131a15d089cc36ceb0671a8e6d6
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2016-02-03 21:18:58 +0100

    Aggiornato AppUser secondo framework

--- components/AppUser.php
+++ components/AppUser.php
@@ -3,32 +3,23 @@
 namespace app\components;
 
 use Yii;
-use yii\web\User;
+use mauriziocingolani\yii2fmwkphp\AppWebUser;
 
 /**
  * Questa classe va impostata come classe per il componente e deve definire
  * i metodi che permettono di stabilire il ruolo di un utente.
  */
-class AppUser extends User {
+class AppUser extends AppWebUser {
 
-    const ROLE_DEVELOPER = 1;
     const ROLE_GUEST = 2;
     const ROLE_EDITOR = 3;
 
-    public function isDeveloper() {
-        return !Yii::$app->user->isGuest && Yii::$app->user->identity->RoleID == self::ROLE_DEVELOPER;
-    }
-
     public function isEditor($includeDeveloper = true) {
-        return !Yii::$app->user->isGuest &&
-                (Yii::$app->user->identity->RoleID == self::ROLE_EDITOR ||
-                ($includeDeveloper && Yii::$app->user->identity->RoleID == self::ROLE_DEVELOPER));
+        return $this->isRole(self::ROLE_EDITOR, $includeDeveloper);
     }
 
     public function isGuest($includeDeveloper = true) {
-        return !Yii::$app->user->isGuest &&
-                (Yii::$app->user->identity->RoleID == self::ROLE_GUEST ||
-                ($includeDeveloper && Yii::$app->user->identity->RoleID == self::ROLE_DEVELOPER));
+        return $this->isRole(self::ROLE_GUEST, $includeDeveloper);
     }
 
 }
